91fbdc68d7ab13b749dadc315ac3dccebb5dd9bd
unknown <l.zuercher@tincan.ch>
2016-12-12 23:33:36 +0100

    fix: some basic cleanup

---
+++ src/js/simulator/Simulator.js
@@ -0,0 +1,28 @@
+/**
+ * Simulates the way from stop one to stop two
+ * Simply enter those two stops.
+ * It uses the A* algorythm
+ */
+
+/**
+ * TODO:
+ * Using astar.js
+ * simulate the graph and nodes -> test
+ */
+
+module.exports = function(stops, lines) {
+  var self = {};
+
+  /**
+   * Lines can only be swaped, when they are defined
+   * as contributers
+   */
+
+  self.getWayPoints(stop1, stop2) {
+
+
+
+  }
+
+  return self;
+};
